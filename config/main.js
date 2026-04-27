ee1431884c161af2cf386d7ac3783429f6d9953b
vishnevskiynathan-cmyk <vishnevskiynathan@gmail.com>
2026-04-27 13:47:24 -0700

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "dayly rust games"; // Change this to change the name of your website.
+var subtext = "best games: cs:go clicker and basket random"; // set the subtext
 
 // more settings in main.css
 
